ad611183e6add249ad35f901873907a1a8b42e8f
Вова Задерецький <zaderetskiyvolodya@gmail.com>
2020-08-11 15:25:10 +0300

    no message

---
+++ HomeWork5/Message.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace HomeWork5
+{
+    static class Message
+    {
+        static public string WordsShorterThan(string str,int n)
+        {
+            string[] words = Regex.Split(str, @"(?<=[\.!\?])\s+");
+            StringBuilder newStr = new StringBuilder();
+            foreach (string word in words)
+            {
+                if (word.Length <= n)
+                {
+                    newStr.Append(word + "  ");
+                }
+            }
+            return newStr.ToString();
+        } 
+
+        static public string DeleteAllWordsWith(string str,char ending)
+        {
+            string[] words = Regex.Split(str, @"(?<=[\.!\?])\s+");
+            StringBuilder newStr = new StringBuilder();
+            foreach (string word in words)
+            {
+                if (word[word.Length-1] != ending)
+                {
+                    newStr.Append(word + "  ");
+                }
+            }
+            return newStr.ToString();
+        }
+
+        static public string LongestWord(string str)
+        {
+            string[] words = Regex.Split(str, @"(?<=[\.!\?])\s+");
+            int index = 0,length = 0;
+            for (int i = 0; i< words.Length; i++)
+            {
+                if (words[i].Length > length)
+                {
+                    index = i;
+                    length = words[i].Length;
+                }
+            }
+            return words[index];
+        }
+
+        static public string AllLongestWord(string str)
+        {
+            string[] words = Regex.Split(str, @"(?<=[\.!\?])\s+");
+           List<int> indexes = new List<int>(); 
+            int length = 0;
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length > length)
+                {
+                    indexes.Clear();
+                    indexes.Add(i);
+                    length = words[i].Length;
+                }else if(words[i].Length == length)
+                {
+                    indexes.Add(i);
+                }
+            }
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < indexes.Count; i++)
+            {
+                sb.Append(words[indexes[i]] + "  ");
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ HomeWork5/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace HomeWork5
+{
+    class Program
+    {
+        // Володимир Задерецький 
+        /*
+          1. Создать программу, которая будет проверять корректность ввода логина. 
+             Корректным логином будет строка от 2 до 10 символов, содержащая только буквы латинского алфавита или цифры,
+             при этом цифра не может быть первой:
+               а) без использования регулярных выражений;
+          2. Разработать статический класс Message, содержащий следующие статические методы для обработки текста:
+                а) Вывести только те слова сообщения,  которые содержат не более n букв.
+                б) Удалить из сообщения все слова, которые заканчиваются на заданный символ.
+                в) Найти самое длинное слово сообщения.
+                г) Сформировать строку с помощью StringBuilder из самых длинных слов сообщения.
+         */
+        public static bool LoginCheck(string login)
+        {
+            bool okLog = false;
+            if (!Char.IsDigit(login[0]) && (login.Length > 1) && (login.Length <= 10))
+            {
+                okLog = true;
+                for (int i = 0; i < login.Length; i++)
+                {
+                    if (((login[i] >= 'A') && (login[i] <= 'Z')) || ((login[i] >= 'a') && (login[i] <= 'z')) || ((login[i] >= '0') && (login[i] <= '9')))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        okLog = false;
+                        break;
+                    }
+                }
+            }
+            return okLog;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter login:");
+            string login = Console.ReadLine();
+            
+             if (LoginCheck(login))
+             {
+                 Console.WriteLine("Ok");
+             }
+             else
+             {
+                 Console.WriteLine("Not Ok");
+             }
+            
+            Console.WriteLine(Message.WordsShorterThan(login, 6));
+            Console.WriteLine(Message.DeleteAllWordsWith(login, 'f'));
+            Console.WriteLine(Message.DeleteAllWordsWith(login, 'f'));
+            Console.WriteLine(Message.LongestWord(login));
+            Console.WriteLine(Message.AllLongestWord(login));
+        }
+    }
+}
